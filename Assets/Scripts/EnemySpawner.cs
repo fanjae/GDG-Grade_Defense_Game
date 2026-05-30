f3c94d04f369af41a97b87828aa2280cb0bb677d
FanJae <hjjang45@gmail.com>
2026-05-30 13:49:08 +0900

    feat : Spawner를 Wave에 등록된 몬스터 그룹을 순서대로 Spawn하도록 구현
    Spawner를 다음과 같은 형태로 변경하였습니다.
    
    - Wave에 등록되어 있는 몬스터 그룹을 순서대로 Spawn.
    - Spawner에서 살아있는 적 수를 추적하고, 모든 적이 제거되는 것을 기다리도록 처리

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,33 +3,50 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [Header("Spawn Enemy Prefab")]
-    [SerializeField] private GameObject[] enemyPrefabs; // 0529 : 배열로 변경
     [Header("Spawn Enemy Position")]
     [SerializeField] private Transform spawnPoint;
-    [Header("Spawn Setting")]
-    [SerializeField] private int spawnCount = 10;
-    [SerializeField] private float spawnInterval = 1.0f;
 
-    private WaitForSeconds waitSpawn;
+    // 현재 웨이브에서 살아있는 적의 수
+    private int aliveEnemyCount; 
 
-    void Start()
+    public IEnumerator SpawnWave(WaveData waveData)
     {
-        waitSpawn = new WaitForSeconds(spawnInterval);
-        StartCoroutine(spawnCo());
-    }
-    IEnumerator spawnCo()
-    {
-        for (int i = 0; i < spawnCount; i++)
+        // 웨이브 시작할 때 생존 적 수를 0으로 재설정
+        aliveEnemyCount = 0; 
+
+        if (spawnPoint == null) yield break;
+        if (waveData == null || waveData.spawnInfos == null) yield break;
+
+        // 웨이브에 등록된 몬스터 그룹을 순서대로 스폰한다.
+        foreach (EnemySpawnInfo spawnInfo in waveData.spawnInfos)
         {
-            if (enemyPrefabs.Length == 0)
-                yield break;
+            if (spawnInfo.enemyPrefab == null) continue;
 
-            GameObject enemyPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+            WaitForSeconds wait = new WaitForSeconds(spawnInfo.interval);
 
-            Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+            // 설정된 수만큼 몬스터를 생성
+            for(int i = 0; i < spawnInfo.count; i++)
+            {
+                GameObject enemyObj = Instantiate(spawnInfo.enemyPrefab, spawnPoint.position, spawnPoint.rotation);
 
-            yield return waitSpawn;
+                // 생성된 Enemy의 제거 이벤트 구독
+                if(enemyObj.TryGetComponent(out Enemy enemy))
+                {
+                    aliveEnemyCount++;
+                    enemy.onRemoved += HandleEnemyRemoved;
+                }
+
+                yield return wait;
+            }
         }
+
+        // 몬스터 스폰이 끝난 뒤, 현재 웨이브의 모든 적이 제거될 때까지 대기
+        yield return new WaitUntil(() => aliveEnemyCount <= 0);
+    }
+    private void HandleEnemyRemoved(Enemy enemy)
+    {
+        // 이벤트 중복 호출 방지를 위해 구독 해제 및 살아있는 적의 수 감소
+        enemy.onRemoved -= HandleEnemyRemoved;
+        aliveEnemyCount--;
     }
 }
