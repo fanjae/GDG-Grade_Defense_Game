b3054211d85ea329da9a6c37aca2d538886f5fff
FanJae <hjjang45@gmail.com>
2026-05-30 23:07:15 +0900

    feat : 타워 발사 쿨타임 계산 UI 추가
    타워 발사 쿨타임을 계산하는 UI를 추가하였습니다.

---
+++ Assets/Scripts/UI/TowerCoolTimeUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerCooldownUI : MonoBehaviour
+{
+    [SerializeField] private Image cooldownFill;
+
+    public void SetCooldown(float value)
+    {
+        if (cooldownFill == null) return ;
+        cooldownFill.fillAmount = Mathf.Clamp01(value); // 0~1값 제한
+    }
+}
