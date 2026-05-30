4f203fce51177e3ace737fce9a04c0ed9ca25432
FanJae <hjjang45@gmail.com>
2026-05-30 22:12:50 +0900

    feat : Life UI 정보 업데이트 스크립트 구현
    Life 관련 UI 정보를 업데이트하는 스크립트를 구현했습니다.

---
+++ Assets/Scripts/UI/LifeUI.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class LifeUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI lifeText;
+
+    public void UpdateLife(int currentLife) // Life 정보 업데이트
+    {
+        if (lifeText == null) return;
+
+        lifeText.text = $"x {currentLife}";
+    }
+}
