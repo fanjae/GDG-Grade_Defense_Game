3ce102fb387aa8dde4469a677cfb461d9b919235
FanJae <hjjang45@gmail.com>
2026-05-30 15:23:57 +0900

    feat : 웨이브 데이터 추가
    몬스터 웨이브 정보 추가
    
    - 하나의 웨이브에 등장해야할 적 스폰 정보 목록을 담은 데이터 추가

---
+++ Assets/Scripts/WaveData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class WaveData // 하나의 웨이브에 등장해야할 적 스폰 정보 목록
+{
+    [SerializeField] private EnemySpawnInfo[] spawnInfos;
+
+    public EnemySpawnInfo[] SpawnInfos => spawnInfos;
+}
