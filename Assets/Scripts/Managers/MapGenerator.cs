e07d6f1d3d5806fb440ca5ef8f470bffd316a953
jack <jack342@naver.com>
2026-05-30 15:47:53 +0900

    맵을 Excle 파일로 작성, 몬스터 WayPoint 및 맵 구현
    Excel 파일을 CSV 파일로 저장해서
    Enum을 통해 프리팹에 있는 BulidTile, Road 등을 게임 오브젝트로 만들어서
    설정된 위치에 스폰되게 했습니다.

---
+++ Assets/Scripts/Managers/MapGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    private MapManager mapManager;
+
+    [SerializeField]
+    private GameObject[] mapObjects;
+
+    [SerializeField]
+    private Vector3 startPos = Vector3.zero;
+
+    private void Awake()
+    {
+        mapManager = GetComponent<MapManager>();
+    }
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+    private void GenerateMap()
+    {
+        for (int i = 0; i < mapManager.MapDatas.Count; i++)
+        {
+            List<MapType> mapTypes = mapManager.MapDatas[i];
+
+            for (int j = 0; j < mapTypes.Count; j++)
+            {
+                float posY = 0.0f;
+
+                if (mapTypes[j] == MapType.BuildTile)
+                {
+                    posY = 0.2f;
+                }
+                if (mapTypes[j] == MapType.Road)
+                {
+                    posY = 0.5f;
+                }
+
+                Vector3 pos = startPos + new Vector3((j * 2), posY, (i * -2));
+                Instantiate(mapObjects[(int)mapTypes[j]], pos, Quaternion.identity, transform);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Managers/MapManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum MapType
+{
+    BuildTile,
+    Road
+}
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset mapFile;
+
+    public Dictionary<int, List<MapType>> MapDatas { get; private set; } = new Dictionary<int, List<MapType>>();
+
+    private void Awake()
+    {
+        MapLoad();
+    }
+
+    private void MapLoad()
+    {
+        string mapData = mapFile.text;
+
+        string[] row = mapData.Split("\r\n");
+
+        for (int i = 0; i < row.Length; i++)
+        {
+            if (row[i] == "")
+            {
+                continue;
+            }
+
+            string[] col = row[i].Split(",");
+
+            List<MapType> mapTypes = new List<MapType>();
+
+            for (int j = 0; j < col.Length; j++)
+            {
+                if (int.TryParse(col[j], out int mapEnumType))
+                {
+                    mapTypes.Add((MapType)mapEnumType);
+                }
+            }
+
+            MapDatas.Add(i, mapTypes);
+        }
+    }
+
+}
