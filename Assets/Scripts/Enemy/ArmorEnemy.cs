63e9dd1d732122f2b2f9f466fcdde092a752d30f
voyager-on-star <es001203@gmail.com>
2026-05-31 17:20:29 +0900

    Sheild/Armor Enemy 추가
    StealthEnemy 프리팹에 스크립트 추가도 했습니다

---
+++ Assets/Scripts/Enemy/ArmorEnemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ArmorEnemy : Enemy
+{
+    private int armorValue = 3;
+    protected override void Reset()
+    {
+        moveSpeed = 1.5f;
+        maxHp = 50;
+        coinValue = 20;
+    }
+    public override void TakeDamage(int damage)
+    {
+        if (armorValue > damage)
+        {
+            return;
+        }
+        base.TakeDamage(damage);
+    }
+}
